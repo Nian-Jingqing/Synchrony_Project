7374d35127657eb0b33cdd543d5cad2182629741
Artur Czeszumski <aczeszumski@uos.de>
2020-12-08 15:33:38 +0100

    add second subject

--- load_physio.m
+++ load_physio.m
@@ -5,5 +5,10 @@ addpath(genpath('D:\Dropbox\Synchrony_Adam'))
 % samplig rate is 1000 Hz, 
 %column 1 == time (ms) 
 %column 2 == raw signal ECG
-d = importdata('SNS_005_11061518_ECG.txt')
+d_sub_1 = importdata('SNS_013L_N_11161318_ECG.txt');
+d_sub_2 = importdata('SNS_014S_N_11161318_ECG.txt');
+%%
+
+
+
 %%
