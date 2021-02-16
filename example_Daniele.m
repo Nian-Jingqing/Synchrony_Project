0fc222347895651158500d0e5936acdacbcfd250
Artur Czeszumski <aczeszumski@uos.de>
2021-02-16 09:35:36 +0100

    short script to show how to open data for Daniele

---
+++ example_Daniele.m
@@ -0,0 +1,15 @@
+%% Load data
+cd D:\Dropbox\Synchrony_Adam
+addpath(genpath('D:\Dropbox\Synchrony_Adam'))
+
+%PCG
+PCG = importdata('SNS_005_11061518_PCG.txt');
+
+%plot
+plot(PCG.data(:,2))
+
+%122100 start
+%start + 150000
+
+
+
