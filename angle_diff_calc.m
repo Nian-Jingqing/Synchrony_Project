4577349966aa7af22892dcedcea5ca945ac0374a
Artur Czeszumski <aczeszumski@uos.de>
2021-03-09 14:38:30 +0100

    changed the script to calculate diff on sudo trials

--- angle_diff_calc.m
+++ angle_diff_calc.m
@@ -8,6 +8,7 @@ angle_diff_RS3 = zeros(23, 44, 24, 24);
 
 
 num_freq = 44;
+trial_size = 500;
 
 for pair = 1:23
     for condition = 1:3
@@ -18,14 +19,23 @@ for pair = 1:23
         
         angle_diff = zeros(num_freq,24,24);
         
+        n = size(tf_elec_sub1,3);
+        trials = n/500;
+        
         for frequencies = 1:num_freq
             angle_diff_freq = zeros(24,24);
             for electrodes_sub1 = 1:24
                 data1 = squeeze(angle(tf_elec_sub1(electrodes_sub1,frequencies,:)));
                 for electrodes_sub2 = 1:24
                     data2 = squeeze(angle(tf_elec_sub2(electrodes_sub2,frequencies,:)));
-                    angle_diff_elec = mean(data1-data2);
-                    angle_diff_freq(electrodes_sub1,electrodes_sub2) = angle_diff_elec;
+                    data_trial_1 = mat2cell(data1,diff([0:trial_size:n-1,n]));
+                    data_trial_2 = mat2cell(data2,diff([0:trial_size:n-1,n]));
+                    trials_angle_diff = zeros(trials,1);
+                    for trial = 1:trials
+                         trials_angle_diff(trial) = mean(angdiff(data_trial_1{trial,1}-data_trial_2{trial,1}));
+                    end
+
+                    angle_diff_freq(electrodes_sub1,electrodes_sub2) = mean(trials_angle_diff);
                 end
             end
             angle_diff(frequencies,:,:) = angle_diff_freq;
