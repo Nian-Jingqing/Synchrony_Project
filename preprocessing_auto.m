614fe7589843fdd9574c0c7dcb831472a51b10fa
Artur Czeszumski <aczeszumski@uos.de>
2021-09-13 14:58:15 +0200

    improve saving file names and repair saving data properly

--- preprocessing_auto.m
+++ preprocessing_auto.m
@@ -1,12 +1,12 @@
 %% Load data
-cd D:\Dropbox\Synchrony_Adam
-addpath(genpath('D:\Dropbox\Synchrony_Adam'))
+cd D:\Dropbox\Projects\Emotional_Sharing_EEG
+addpath(genpath('D:\Dropbox\Projects\Emotional_Sharing_EEG'))
 eeglabpath = fileparts(which('eeglab.m'));
 % load eeglab
 eeglab;
 
 
-cd D:\Dropbox\Synchrony_Adam\EEG_Data\LSL
+cd D:\Dropbox\Projects\Emotional_Sharing_EEG\EEG_Data\LSL_September
 list_of_files = dir('**/*.xdf');
 
 % files_info = [];
@@ -73,7 +73,7 @@ list_of_files = dir('**/*.xdf');
 for eeg_file = 1:size(list_of_files)
 
     path_to_file = [list_of_files(eeg_file).folder '\' list_of_files(eeg_file).name];
-    path_new_folder = sprintf('D:\\Dropbox\\Synchrony_Adam\\EEG_Data\\LSL\\%s_MOBI', list_of_files(eeg_file).name);
+    path_new_folder = sprintf('D:\\Dropbox\\Projects\\Emotional_Sharing_EEG\\EEG_Data\\LSL_September\\%s_MOBI', list_of_files(eeg_file).name);
 
     mobilab.allStreams = dataSourceXDF(path_to_file, path_new_folder);
     all_mobilab_streams = [mobilab.allStreams().item];
@@ -136,8 +136,6 @@ for eeg_file = 1:size(list_of_files)
 
 % add events to both datasets (300 events separated by 1 second
 eeg_sub1_chanlocs = eeg_sub1.chanlocs;
-
-
 eeg_sub1.data(25,[1000:500:500*floor(size(eeg_sub1.data,2)/500)]) = 1; % simulating a stimulus onset every second
 eeg_sub1 = pop_chanevent(eeg_sub1, 25,'edge','leading','edgelen',0,'duration','on','nbtype',1);
 eeg_sub1.chanlocs = eeg_sub1_chanlocs;
@@ -174,7 +172,7 @@ for sub = 1:2
     d_tmp = nt_zapline(d_tmp, power_line/srate);
     EEG.data = permute(d_tmp,[2,1]);
     
-    EEG.setname = EEG.filepath(40:end);
+    EEG.setname = EEG.filepath(66:end);
     EEG.setname = EEG.setname(1:end-9);
     
     
@@ -216,7 +214,7 @@ for sub = 1:2
     % high pass 2 Hz for data used for ICA calculations
     eeg_tmp = pop_eegfiltnew(EEG, 2, []);   % highpass  2 Hz to not include slow drifts
     % create amica folder
-    cd D:\Dropbox\Synchrony_Adam\EEG_Data\Preprocessed_July
+    cd D:\Dropbox\Projects\Emotional_Sharing_EEG\EEG_Data\Preprocessed_September
     mkdir(sprintf('ICA_amica_%s_%d',EEG.setname, sub))
     outDir = what(sprintf('ICA_amica_%s_%d',EEG.setname,sub));
     %run ICA
@@ -273,7 +271,7 @@ for sub = 1:2
     EEG = pop_interp(EEG, full_chanlocs,'spherical');
     
     EEG = pop_editset(EEG, 'setname', sprintf('cleaned_%s_sub_%d', EEG.setname, sub));
-    EEG = pop_saveset(EEG, 'filename', sprintf('cleaned_%s_sub_%d', EEG.setname, sub));
+    EEG = pop_saveset(EEG, 'filename', EEG.setname);
     if sub == 1
         eeg_sub1 = EEG;
     elseif sub == 2
@@ -324,8 +322,8 @@ eeg_sub2.data = reshape(eeg_sub2.data, 24, []);
 [eeg_sub1.event.latency] = latency_sub1{:};
 [eeg_sub2.event.latency] = latency_sub2{:};
 
-eeg_sub1.trials =[];
-eeg_sub2.trials =[];
+eeg_sub1.trials = size(eeg_sub1.data,2)/500;
+eeg_sub2.trials = size(eeg_sub2.data,2)/500;
 
 
 % i commented out line 495 in the script 'po_editset': '%EEGOUT =
@@ -336,9 +334,9 @@ eeg_sub2.trials =[];
 % and line 132 in pop_saveset
 
 eeg_sub1 = pop_editset(eeg_sub1, 'setname', sprintf('hyper_%s', eeg_sub1.setname));
-eeg_sub1 = pop_saveset(eeg_sub1, 'filename', sprintf('hyper_%s', eeg_sub1.setname), 'check', 'off');
+eeg_sub1 = pop_saveset(eeg_sub1, 'filename', eeg_sub1.setname);
 eeg_sub2 = pop_editset(eeg_sub2, 'setname', sprintf('hyper_%s', eeg_sub2.setname));
-eeg_sub2 = pop_saveset(eeg_sub2, 'filename', sprintf('hyper_%s', eeg_sub2.setname), 'check', 'off');
+eeg_sub2 = pop_saveset(eeg_sub2, 'filename', eeg_sub2.setname);
 
     
 end
