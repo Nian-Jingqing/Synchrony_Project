a50abe11d3764d05b1f636e06b271186b361f147
Til <tilman.kalthoff@me.com>
2021-09-14 13:49:28 +0200

    setup artur vs. til filepaths

--- check_preprocessing.m
+++ check_preprocessing.m
@@ -1,13 +1,23 @@
 %% Load data
-cd D:\Dropbox\Projects\Emotional_Sharing_EEG
-addpath(genpath('D:\Dropbox\Projects\Emotional_Sharing_EEG'))
+if strcmp(getenv('USER'),'til')
+    cd /Users/til/Uni/Master/_Ma.Thesis
+    addpath(genpath('/Users/til/Uni/Master/_Ma.Thesis'))
+else
+    cd D:\Dropbox\Projects\Emotional_Sharing_EEG
+    addpath(genpath('D:\Dropbox\Projects\Emotional_Sharing_EEG'))
+end
+
 eeglabpath = fileparts(which('eeglab.m'));
 % load eeglab
 eeglab;
 
-cd D:\Dropbox\Projects\Emotional_Sharing_EEG\EEG_Data\Preprocessed_July\hyper_cleaned
+if strcmp(getenv('USER'),'til')
+    cd /Users/til/Uni/Master/_Ma.Thesis/project_data/preprocessed_july/hyper_cleaned
+else
+    cd D:\Dropbox\Projects\Emotional_Sharing_EEG\EEG_Data\Preprocessed_July\hyper_cleaned
+end
 list_of_files = dir('**/*.set');
 
-for
 
-EEG = pop_loadset('filename', list_of_files(1).name,'check', 'off', 'loadmode', 'info');
+
+EEG = pop_loadset('filename', list_of_files(1).name);
