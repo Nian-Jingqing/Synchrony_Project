faf7912031035fd86cd0bc0acbeca0560a460e09
Til <tilman.kalthoff@me.com>
2021-10-14 14:48:22 +0200

    skript for all pairs/freqs/elecs/elecs Saves as separate file

--- power_corr_til.m
+++ power_corr_til.m
@@ -1,16 +1,3 @@
-%% Set Parameters
-n_pairs = 39;
-n_frex = 44;
-n_elex = 24;
-
-conditions = ['RS1' 'NS' 'RS2' 'ES' 'RS3'];
-%% Initialize matrizes
-
-pow_cor_RS1 = zeros(n_pairs,n_frex,n_elex,n_elex);
-pow_cor_NS  = zeros(n_pairs,n_frex,n_elex,n_elex);
-pow_cor_RS2 = zeros(n_pairs,n_frex,n_elex,n_elex);
-pow_cor_ES  = zeros(n_pairs,n_frex,n_elex,n_elex);
-pow_cor_RS3 = zeros(n_pairs,n_frex,n_elex,n_elex);
 
 %% Load Data
 help_chose_analysisfolder % get filepath
@@ -57,37 +44,75 @@ if(length(pairS) ~= length(pairL))
     error('Not equal amounts of speakers and listeners');
 end
 
-%% for each pair
+
+%% Set Parameters
+n_pairs = length(pairS);
+n_frex = 44;
+n_elex = 24;
+
+conditions = {'RS1' 'NS' 'RS2' 'ES' 'RS3'};
+%% Initialize matrizes
+
+pow_cor_RS1 = cell(n_pairs,n_frex,n_elex,n_elex);
+pow_cor_NS  = cell(n_pairs,n_frex,n_elex,n_elex);
+pow_cor_RS2 = cell(n_pairs,n_frex,n_elex,n_elex);
+pow_cor_ES  = cell(n_pairs,n_frex,n_elex,n_elex);
+pow_cor_RS3 = cell(n_pairs,n_frex,n_elex,n_elex);
+
+%% Power Correlation
+
+% check system to get correct filepath
+if strcmp(getenv('USER'),'til')
+    filepath = '/Volumes/Tils Passport/Uni/MasterthesisData/TF';
+else
+    filepath = '';
+end
+
+cd(filepath);
+addpath(genpath(filepath))
+
+% Loopchain: pair - condition - frex - elex
+% for each pair
 for pair = 1:length(pairS)
     % for each condition
+    tic
     for cond = 1:length(conditions)
         
-        pow_cor = zeros(n_freq,n_elex,n_elex);
+        % matrix to be filled
+        pow_cor = cell(n_frex,n_elex,n_elex);
         
-        tf_S = load(sprintf('tf_subject%s_roleS_conditinon%s.mat',pairS{pair},conditions(cond)));
-        tf_L = load(sprintf('tf_subject%s_roleL_conditinon%s.mat',pairL{pair},conditions(cond)));
-        
+        % load current condition for each subject of current pair
+        tf_S = load(sprintf('tf_subject%s_roleS_condition%s.mat',pairS{pair},conditions{cond}));
+        tf_L = load(sprintf('tf_subject%s_roleL_condition%s.mat',pairL{pair},conditions{cond}));
+        fprintf('pair %d of %d loaded',pair,length(pairS));
         % for each freq
         for freq = 1:n_frex
-            pow_cor_freq = zeros(n_elex,n_elex);
+            
+            % matrix to be filled
+            pow_cor_freq = cell(n_elex,n_elex);
+            
             % for each elec Speaker
             for elecS = 1:n_elex
                 % for eaach elec Listener
                 for elecL = 1:n_elex    
                     
                     % get Data & extract power
-                    pow_S = abs(tf_S(elecS,freq,:)).^2;
-                    pow_L = abs(tf_L(elecL,freq,:)).^2;
+                    pow_S = abs(tf_S.tf_elec(elecS,freq,:)).^2;
+                    pow_L = abs(tf_L.tf_elec(elecL,freq,:)).^2;
                     
-                    % DO CORRELATION
-                    pow_cor_freq(elecS,elecL) = mean(corrcoef(pow_S,pow_L));
+                    % Correlation - spearman since no normal distribution
+                    [p,r]= corr(pow_S,pow_L,'type','spearman');
+                    
+                    % store r and p values in cell
+                    pow_cor_freq(elecS,elecL) = {[p,r]};
+                    
                 end 
             end
             pow_cor(freq,:,:) = pow_cor_freq;
         end
         
         % fill preinitialized matrizes
-        switch condition(cond)
+        switch conditions{cond}
             case 'RS1'
                 pow_cor_RS1(pair,:,:,:) = pow_cor;
             case 'NS'
@@ -99,13 +124,28 @@ for pair = 1:length(pairS)
             case 'RS3'
                 pow_cor_RS3(pair,:,:,:) = pow_cor;
         end
+        
     end
+    % check progress
+    fprintf('pair %d of %d done',pair,length(pairS));
+    toc
 end
 
 
 %% Save matrices
 % navigate to folder
 
+% check system to get correct filepath
+if strcmp(getenv('USER'),'til')
+    filepath = '/Volumes/Tils Passport/Uni/MasterthesisData/pow_corr_single';
+else
+    filepath = '';
+end
+
+cd(filepath);
+addpath(genpath(filepath))
+
+% save all conditions
 save('pow_cor_RS1.mat', 'pow_cor_trial_RS1','-v7.3');
 save('pow_cor_NS.mat', 'pow_cor_trial_NS','-v7.3');
 save('pow_cor_RS2.mat', 'pow_cor_trial_RS2','-v7.3');
