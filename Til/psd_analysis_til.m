c8053a319e36439b23a928c4711e96b4d074edf8
Artur Czeszumski <aczeszumski@uos.de>
2021-11-09 11:29:52 +0100

    change to continuous data, change to fft

--- Til/psd_analysis_til.m
+++ Til/psd_analysis_til.m
@@ -21,7 +21,8 @@ Power_mat.power = [];
 % loop over recordings
 for i = 1:numel(list_of_files)
     % load next file
-    EEG = pop_loadset('filename', list_of_files(i).name,'verbose','off');
+    EEG = pop_loadset('filename', list_of_files(i).name);
+    EEG = eeg_epoch2continuous(EEG); % change to continouous
     
     % get and save subject information
     [subj, role, cond] = help_subjectinfo(EEG.setname);
@@ -29,20 +30,30 @@ for i = 1:numel(list_of_files)
     Power_mat(i).role = role;
     Power_mat(i).cond = cond;
     
-    % calculate power for each channel and frequency
-    power = zeros(EEG.nbchan,nfreqs);
-    for ch = 1:EEG.nbchan
-        [spectra, freqs] = spectopo(EEG.data(ch,:,:),0,EEG.srate,'freqrange',[0 nfreqs],'plot','off','verbose','off');
-        % only take the specified n freqs
-        spectra = spectra(1:nfreqs);
-        freqs = freqs(1:nfreqs);
-        %save power of each channel
-        power(ch,:) = spectra;
-    end
+    chanpowr = ( 2*abs( fft(EEG.data/EEG.pnts ))).^2;
+    hz = linspace(0,EEG.srate/2,floor(EEG.pnts/2)+1);
+    
+
+%     % calculate power for each channel and frequency
+%     power = zeros(EEG.nbchan,nfreqs);
+%     for ch = 1:EEG.nbchan
+%         %[spectra, freqs] = spectopo(EEG.data(ch,:,:),0,EEG.srate,'freqrange',[0 nfreqs],'plot','off','verbose','off');
+%         powspectAverage  = fft(squeeze(mean(EEG.data(ch,:,:),3)))/length(EEG.times);
+%         powspectAverage  = 2*abs(powspectAverage);
+%         powspectAverage = powspectAverage(1:nfreqs);
+%         
+%         % only take the specified n freqs
+%         %spectra = spectra(1:nfreqs);
+%         %freqs = freqs(1:nfreqs);
+%         %save power of each channel
+%         power(ch,:) = powspectAverage;
+%     end
     
     % save power/freq matrix in struct
-    Power_mat(i).power = power;
-    
+    Power_mat(i).power =  chanpowr(:,1:find(hz == 45));
+    plot(hz(1:find(hz == 45)), Power_mat(2).power')
     % display progress (0 to 1)
     disp(i/numel(list_of_files));
 end
+
+eegplot(EEG.data,'srate',EEG.srate,'eloc_file',EEG.chanlocs)
