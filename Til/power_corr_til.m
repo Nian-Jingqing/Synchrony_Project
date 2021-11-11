223144dd6898a1575b5eedbec5be02ed72eaf305
Til <tilman.kalthoff@me.com>
2021-11-11 14:37:27 +0100

    fixed saving

--- Til/power_corr_til.m
+++ Til/power_corr_til.m
@@ -1,3 +1,12 @@
+%% Script computes single Pearson correlation 
+% for each pair - 37
+% for each condition - 5
+% for each frequency - 44
+% for each electrode pair 24x24
+
+% set filepath for loading and saving
+filepath_loading = '/Volumes/til_uni/Uni/MasterthesisData/TF';
+filepath_saving = '/Volumes/til_uni/Uni/MasterthesisData/sliding_pow_corr_reduced';
 
 %% Setup
 fprintf('Setup');
@@ -13,11 +22,17 @@ n_elex = 24;
 conditions = {'RS1' 'NS' 'RS2' 'ES' 'RS3'};
 
 % Initialize matrizes
-pow_cor_RS1 = cell(n_pairs,n_frex,n_elex,n_elex);
-pow_cor_NS  = cell(n_pairs,n_frex,n_elex,n_elex);
-pow_cor_RS2 = cell(n_pairs,n_frex,n_elex,n_elex);
-pow_cor_ES  = cell(n_pairs,n_frex,n_elex,n_elex);
-pow_cor_RS3 = cell(n_pairs,n_frex,n_elex,n_elex);
+pow_corr_p_RS1 = cell(n_pairs,n_frex,n_elex,n_elex);
+pow_corr_p_NS  = cell(n_pairs,n_frex,n_elex,n_elex);
+pow_corr_p_RS2 = cell(n_pairs,n_frex,n_elex,n_elex);
+pow_corr_p_ES  = cell(n_pairs,n_frex,n_elex,n_elex);
+pow_corr_p_RS3 = cell(n_pairs,n_frex,n_elex,n_elex);
+
+pow_corr_r_RS1 = cell(n_pairs,n_frex,n_elex,n_elex);
+pow_corr_r_NS  = cell(n_pairs,n_frex,n_elex,n_elex);
+pow_corr_r_RS2 = cell(n_pairs,n_frex,n_elex,n_elex);
+pow_corr_r_ES  = cell(n_pairs,n_frex,n_elex,n_elex);
+pow_corr_r_RS3 = cell(n_pairs,n_frex,n_elex,n_elex);
 
 fprintf(' - done\n');
 %% Power Correlation
@@ -32,29 +47,26 @@ end
 cd(filepath);
 addpath(genpath(filepath))
 
-% Loopchain: pair - condition - frex - elex
-% for each pair
+% Loopchain: pair - condition - frequencies - electrodes
 for pair = 1:length(pairS)
-    % for each condition
     tic
     for cond = 1:length(conditions)
         
         % matrix to be filled
-        pow_cor = cell(n_frex,n_elex,n_elex);
+        pow_corr_r = zeros(n_frex,n_elex,n_elex);
+        pow_corr_p = zeros(n_frex,n_elex,n_elex);
         
         % load current condition for each subject of current pair
         tf_S = load(sprintf('tf_subject%s_roleS_condition%s.mat',pairS{pair},conditions{cond}));
         tf_L = load(sprintf('tf_subject%s_roleL_condition%s.mat',pairL{pair},conditions{cond}));
         fprintf('pair %d of %d loaded',pair,length(pairS));
-        % for each freq
         for freq = 1:n_frex
             
             % matrix to be filled
-            pow_cor_freq = cell(n_elex,n_elex);
+            pow_corr_freq_r = zeros(n_elex,n_elex);
+            pow_corr_freq_p = zeros(n_elex,n_elex);
             
-            % for each elec Speaker
             for elecS = 1:n_elex
-                % for eaach elec Listener
                 for elecL = 1:n_elex    
                     
                     % get Data & extract power
@@ -64,54 +76,54 @@ for pair = 1:length(pairS)
                     % Correlation - spearman since no normal distribution
                     [r,p]= corr(squeeze(pow_S),squeeze(pow_L),'type','spearman');
                     
-                    % store r and p values in cell
-                    pow_cor_freq(elecS,elecL) = {[r,p]};
+                    % store r and p values in array
+                    pow_corr_freq_r(elecS,elecL) = r;
+                    pow_corr_freq_p(elecS,elecL) = p;
                     
                 end 
-            end
-            pow_cor(freq,:,:) = pow_cor_freq;
-        end
+            end % electrode loops
+            pow_corr_r(freq,:,:) = pow_corr_freq_r;
+            pow_corr_p(freq,:,:) = pow_corr_freq_p;
+        end % frequency loop
         
         % fill preinitialized matrizes
         switch conditions{cond}
             case 'RS1'
-                pow_cor_RS1(pair,:,:,:) = pow_cor;
+                pow_corr_r_RS1(pair,:,:,:) = pow_corr_r;
+                pow_corr_p_RS1(pair,:,:,:) = pow_corr_p;
             case 'NS'
-                pow_cor_NS(pair,:,:,:)  = pow_cor;
+                pow_corr_r_NS(pair,:,:,:)  = pow_corr_r;
+                pow_corr_p_NS(pair,:,:,:)  = pow_corr_p;
             case 'RS2'
-                pow_cor_RS2(pair,:,:,:) = pow_cor;
+                pow_corr_r_RS2(pair,:,:,:) = pow_corr_r;
+                pow_corr_p_RS2(pair,:,:,:) = pow_corr_p;
             case 'ES'
-                pow_cor_ES(pair,:,:,:)  = pow_cor;
+                pow_corr_r_ES(pair,:,:,:)  = pow_corr_r;
+                pow_corr_p_ES(pair,:,:,:)  = pow_corr_p;
             case 'RS3'
-                pow_cor_RS3(pair,:,:,:) = pow_cor;
+                pow_corr_r_RS3(pair,:,:,:) = pow_corr_r;
+                pow_corr_p_RS3(pair,:,:,:) = pow_corr_p;
         end
-        
-    end
+       
+    end % condition loop
     % check progress
     fprintf('pair %d of %d done',pair,length(pairS));
     toc
-end
+end % pair loop
 
 
 %% Save matrices
 % navigate to folder
 
-% check system to get correct filepath
-if strcmp(getenv('USER'),'til')
-    filepath = '/Volumes/Tils Passport/Uni/MasterthesisData/pow_corr_single';
-else
-    filepath = '';
-end
-
-cd(filepath);
-addpath(genpath(filepath))
+cd(filepath_saving);
+addpath(genpath(filepath_saving))
 
 % save all conditions
-save('pow_cor_RS1.mat', 'pow_cor_RS1','-v7.3');
-save('pow_cor_NS.mat', 'pow_cor_NS','-v7.3');
-save('pow_cor_RS2.mat', 'pow_cor_RS2','-v7.3');
-save('pow_cor_ES.mat', 'pow_cor_ES','-v7.3');
-save('pow_cor_RS3.mat', 'pow_cor_RS3','-v7.3');
+save('pow_corr_RS1.mat', 'pow_corr_RS1','-v7.3');
+save('pow_corr_NS.mat', 'pow_corr_NS','-v7.3');
+save('pow_corr_RS2.mat', 'pow_corr_RS2','-v7.3');
+save('pow_corr_ES.mat', 'pow_corr_ES','-v7.3');
+save('pow_corr_RS3.mat', 'pow_corr_RS3','-v7.3');
 
 
 %% Helperfunctions
@@ -167,3 +179,5 @@ function [pairS, pairL] = get_pairs()
         error('Not equal amounts of speakers and listeners');
     end
 end
+
+
