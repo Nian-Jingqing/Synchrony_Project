c01d3a854bc6acb70d2297f8ff6af25e6d81d328
Artur Czeszumski <aczeszumski@uos.de>
2021-11-18 16:47:29 +0100

    tmp stuff

--- helper_tmp.m
+++ helper_tmp.m
@@ -4,3 +4,24 @@ for i=1:37
 end
 
 
+
+
+EEG = pop_loadset();
+
+
+% play with circular-circular and circular lineartmp_v = [];
+for j=1:10000
+    vector1 = [];
+    vector2 = [];
+    for i= 1:100
+        mnval = -pi;
+        mxval = pi;
+        val1 = mnval + rand*(mxval-mnval);
+        val2 = mnval + rand*(mxval-mnval);
+        vector1 = [vector1 val1];
+        vector2 = [vector2 val2];
+        
+    end
+    tmp = circ_corrcl(vector1, vector2);
+    tmp_v = [tmp_v tmp];
+end
