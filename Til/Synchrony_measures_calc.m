f3e421bff0f4faa828fcef4600e2807b23e223a9
Artur Czeszumski <aczeszumski@uos.de>
2021-11-29 14:19:29 +0100

    improve ispc calc

--- Til/Synchrony_measures_calc.m
+++ Til/Synchrony_measures_calc.m
@@ -81,11 +81,24 @@ ccorr_rho_RS2  = zeros(n_pairs,n_bands,n_ROI);
 ccorr_rho_ES   = zeros(n_pairs,n_bands,n_ROI);
 ccorr_rho_RS3  = zeros(n_pairs,n_bands,n_ROI);
 
-pow_cor_r_RS1 = cell(n_pairs,n_bands,n_ROI);
-pow_cor_r_NS  = cell(n_pairs,n_bands,n_ROI);
-pow_cor_r_RS2 = cell(n_pairs,n_bands,n_ROI);
-pow_cor_r_ES  = cell(n_pairs,n_bands,n_ROI);
-pow_cor_r_RS3 = cell(n_pairs,n_bands,n_ROI);
+pow_cor_r_RS1 = zeros(n_pairs,n_bands,n_ROI);
+pow_cor_r_NS  = zeros(n_pairs,n_bands,n_ROI);
+pow_cor_r_RS2 = zeros(n_pairs,n_bands,n_ROI);
+pow_cor_r_ES  = zeros(n_pairs,n_bands,n_ROI);
+pow_cor_r_RS3 = zeros(n_pairs,n_bands,n_ROI);
+
+ISPC_RS1 = zeros(n_pairs,n_bands,n_ROI);
+ISPC_NS  = zeros(n_pairs,n_bands,n_ROI);
+ISPC_RS2 = zeros(n_pairs,n_bands,n_ROI);
+ISPC_ES  = zeros(n_pairs,n_bands,n_ROI);
+ISPC_RS3 = zeros(n_pairs,n_bands,n_ROI);
+
+am_env_RS1 = zeros(n_pairs,n_bands,n_ROI);
+am_env_NS  = zeros(n_pairs,n_bands,n_ROI);
+am_env_RS2 = zeros(n_pairs,n_bands,n_ROI);
+am_env_ES  = zeros(n_pairs,n_bands,n_ROI);
+am_env_RS3 = zeros(n_pairs,n_bands,n_ROI);
+
 %% CCorr
 
 % Loopchain: Pairs-Conditions-frequencybands-electrodes
@@ -109,12 +122,16 @@ for pair = 1:n_pairs
         % prepare arrays
         ccorr_rho  = zeros(n_bands,n_ROI);
         pow_cor = zeros(n_bands,n_ROI);
+        ISPC = zeros(n_bands,n_ROI);
+        am_env = zeros(n_bands, n_ROI);
         
         for band = 1:n_bands
             
             % prepare arrays
             ccorr_rho_band  = zeros(1,n_ROI);
             pow_cor_band = zeros(1,n_ROI);
+            ISPC_band = zeros(1,n_ROI);
+            am_env_band = zeros(1,n_ROI);
             
             for ROI = 1:n_ROI
                 
@@ -130,40 +147,65 @@ for pair = 1:n_pairs
                 data_S = squeeze(mean(data_S,1));
                 data_L = squeeze(mean(data_L,1));
                 
+                amp_S = abs(data_S);
+                amp_L = abs(data_L);
+                
+                am_env_S = envelope(amp_S);
+                am_env_L = envelope(amp_L);
+                
+                
                 pow_S = abs(data_S).^2;
                 pow_L = abs(data_L).^2;
                 
-                ccorr_S = squeeze(angle(data_S));
-                ccorr_L = squeeze(angle(data_L));
+                angle_S = squeeze(angle(data_S));
+                angle_L = squeeze(angle(data_L));
                 
                 
-                data_trial_S_ccorr = mat2cell(ccorr_S,1,diff([0:trial_size:n-1,n])); % make 1 second epochs
-                data_trial_L_ccorr = mat2cell(ccorr_L,1,diff([0:trial_size:n-1,n])); % make 1 second epochs
+                data_trial_S_angle = mat2cell(angle_S,1,diff([0:trial_size:n-1,n])); % make 1 second epochs
+                data_trial_L_angle = mat2cell(angle_L,1,diff([0:trial_size:n-1,n])); % make 1 second epochs
                 
                 data_trial_S_pow = mat2cell(pow_S,1,diff([0:trial_size:n-1,n])); % make 1 second epochs
                 data_trial_L_pow = mat2cell(pow_L,1,diff([0:trial_size:n-1,n])); % make 1 second epochs
                 
+                data_trial_S_am_env = mat2cell(am_env_S,1,diff([0:trial_size:n-1,n])); % make 1 second epochs
+                data_trial_L_am_env = mat2cell(am_env_L,1,diff([0:trial_size:n-1,n])); % make 1 second epochs
+                
                 trials_ccorr_trial = zeros(trials,1);
                 trials_pow_cor_trial = zeros(trials,1);
+                trials_ISPC_trial = zeros(trials,1);
+                trials_am_env_trial = zeros(trials,1);
+                
                 for trial = 1:trials
                     %ccorr
-                    rho_ccorr = circ_corrcl(data_trial_S_ccor{trial},data_trial_L_ccorr{trial});
+                    rho_ccorr = circ_corrcl(data_trial_S_angle{trial},data_trial_L_angle{trial});
                     trials_ccorr_trial(trial) = rho_ccorr;
                     %pow cor
-                    r_pow_cor = corr(data_trial_S_pow{trial},data_trial_L_pow{trial},'type','spearman');
+                    r_pow_cor = corr(data_trial_S_pow{trial}',data_trial_L_pow{trial}','type','spearman');
                     trials_pow_cor_trial(trial) = r_pow_cor;
-                    
+                    % ISPC/PLV
+                    diffs = cell2mat(data_trial_S_angle(trial)) - cell2mat(data_trial_L_angle(trial));  % Get Difference (order is irrelevant)
+                    euler_diffs = exp(1i*diffs); % Eulerize angles
+                    mean_diff = mean(euler_diffs); % mean vector
+                    ISPC_value = abs(mean_diff); % ISPC = length of vector (Synchronisation value)
+                    trials_ISPC_trial(trial) = ISPC_value;
+                    %AM envelop cor
+                    r_am_env = corr(data_trial_S_am_env{trial}',data_trial_L_am_env{trial}','type','pearson');
+                    trials_am_env_trial(trial) = r_am_env; 
                 end
                 
                 
                 ccorr_rho_band(ROI)  = mean(trials_ccorr_trial);
                 pow_cor_band(ROI) = mean(trials_pow_cor_trial);
+                ISPC_band(ROI) = mean(trials_ISPC_trial);
+                am_env_band(ROI) = mean(trials_am_env_trial);
                 
                 
             end % electrode loop
             
             ccorr_rho(band,:)  = ccorr_rho_band;
             pow_cor(band,:) = pow_cor_band;
+            ISPC(band,:) = ISPC_band;
+            am_env(band,:) = am_env_band; 
             
         end % frequency band loop
         
@@ -172,22 +214,32 @@ for pair = 1:n_pairs
             case 'RS1'
                 ccorr_rho_RS1(pair,:,:,:)  = ccorr_rho;
                 pow_cor_r_RS1(pair,:,:,:)  = pow_cor;
-                
+                ISPC_RS1(pair,:,:,:)  = ISPC;
+                am_env_RS1(pair,:,:,:) = am_env;
+              
             case 'NS'
                 ccorr_rho_NS(pair,:,:,:)   = ccorr_rho;
                 pow_cor_r_NS(pair,:,:,:)  = pow_cor;
+                ISPC_NS(pair,:,:,:)  = ISPC;
+                am_env_NS(pair,:,:,:) = am_env;
                 
             case 'RS2'
                 ccorr_rho_RS2(pair,:,:,:)  = ccorr_rho;
                 pow_cor_r_RS2(pair,:,:,:)  = pow_cor;
+                ISPC_RS2(pair,:,:,:)  = ISPC;
+                am_env_RS2(pair,:,:,:) = am_env;
                 
             case 'ES'
                 ccorr_rho_ES(pair,:,:,:)   = ccorr_rho;
                 pow_cor_r_ES(pair,:,:,:)  = pow_cor;
+                ISPC_ES(pair,:,:,:)  = ISPC;
+                am_env_ES(pair,:,:,:) = am_env;
                 
             case 'RS3'
                 ccorr_rho_RS3(pair,:,:,:)  = ccorr_rho;
                 pow_cor_r_RS3(pair,:,:,:)  = pow_cor;
+                ISPC_RS3(pair,:,:,:)  = ISPC;
+                am_env_RS3(pair,:,:,:) = am_env;
                 
         end
         fprintf(' - done\n');
@@ -217,29 +269,93 @@ end % pair loop
 % %fprintf(' - done\n');
 % 
 % 
-% % %save csv
+% %save csv
 % cd D:\Dropbox\Projects\Emotional_Sharing_EEG\EEG_Data\ccor\csv_files_ccor
 % % 
 % % 
+% % ccorr
+% csvwrite('R3_theta_ccorr.csv', squeeze(ccorr_rho_RS3(:,1,:)))
+% csvwrite('R3_alpha_ccorr.csv', squeeze(ccorr_rho_RS3(:,2,:)))
+% csvwrite('R3_beta__ccorr.csv', squeeze(ccorr_rho_RS3(:,3,:)))
 % 
-% csvwrite('R3_theta.csv', squeeze(ccorr_rho_RS3(:,1,:)))
-% csvwrite('R3_alpha.csv', squeeze(ccorr_rho_RS3(:,2,:)))
-% csvwrite('R3_beta.csv', squeeze(ccorr_rho_RS3(:,3,:)))
+% csvwrite('R2_theta_ccorr.csv', squeeze(ccorr_rho_RS2(:,1,:)))
+% csvwrite('R2_alpha_ccorr.csv', squeeze(ccorr_rho_RS2(:,2,:)))
+% csvwrite('R2_beta__ccorr.csv', squeeze(ccorr_rho_RS2(:,3,:)))
 % 
-% csvwrite('R2_theta.csv', squeeze(ccorr_rho_RS2(:,1,:)))
-% csvwrite('R2_alpha.csv', squeeze(ccorr_rho_RS2(:,2,:)))
-% csvwrite('R2_beta.csv', squeeze(ccorr_rho_RS2(:,3,:)))
+% csvwrite('R1_theta_ccorr.csv', squeeze(ccorr_rho_RS1(:,1,:)))
+% csvwrite('R1_alpha_ccorr.csv', squeeze(ccorr_rho_RS1(:,2,:)))
+% csvwrite('R1_beta__ccorr.csv', squeeze(ccorr_rho_RS1(:,3,:)))
 % 
-% csvwrite('R1_theta.csv', squeeze(ccorr_rho_RS1(:,1,:)))
-% csvwrite('R1_alpha.csv', squeeze(ccorr_rho_RS1(:,2,:)))
-% csvwrite('R1_beta.csv', squeeze(ccorr_rho_RS1(:,3,:)))
+% csvwrite('ES_theta_ccorr.csv', squeeze(ccorr_rho_ES(:,1,:)))
+% csvwrite('ES_alpha_ccorr.csv', squeeze(ccorr_rho_ES(:,2,:)))
+% csvwrite('ES_beta__ccorr.csv', squeeze(ccorr_rho_ES(:,3,:)))
 % 
-% csvwrite('ES_theta.csv', squeeze(ccorr_rho_ES(:,1,:)))
-% csvwrite('ES_alpha.csv', squeeze(ccorr_rho_ES(:,2,:)))
-% csvwrite('ES_beta.csv', squeeze(ccorr_rho_ES(:,3,:)))
+% csvwrite('NS_theta_ccorr.csv', squeeze(ccorr_rho_NS(:,1,:)))
+% csvwrite('NS_alpha_ccorr.csv', squeeze(ccorr_rho_NS(:,2,:)))
+% csvwrite('NS_beta__ccorr.csv', squeeze(ccorr_rho_NS(:,3,:)))
 % 
-% csvwrite('NS_theta.csv', squeeze(ccorr_rho_NS(:,1,:)))
-% csvwrite('NS_alpha.csv', squeeze(ccorr_rho_NS(:,2,:)))
-% csvwrite('NS_beta.csv', squeeze(ccorr_rho_NS(:,3,:)))
+% % pow_cor
+% csvwrite('R3_theta_p_cor.csv', squeeze(pow_cor_r_RS3(:,1,:)))
+% csvwrite('R3_alpha_p_cor.csv', squeeze(pow_cor_r_RS3(:,2,:)))
+% csvwrite('R3_beta__p_cor.csv', squeeze(pow_cor_r_RS3(:,3,:)))
+% 
+% csvwrite('R2_theta_p_cor.csv', squeeze(pow_cor_r_RS2(:,1,:)))
+% csvwrite('R2_alpha_p_cor.csv', squeeze(pow_cor_r_RS2(:,2,:)))
+% csvwrite('R2_beta__p_cor.csv', squeeze(pow_cor_r_RS2(:,3,:)))
+% 
+% csvwrite('R1_theta_p_cor.csv', squeeze(pow_cor_r_RS1(:,1,:)))
+% csvwrite('R1_alpha_p_cor.csv', squeeze(pow_cor_r_RS1(:,2,:)))
+% csvwrite('R1_beta__p_cor.csv', squeeze(pow_cor_r_RS1(:,3,:)))
+% 
+% csvwrite('ES_theta_p_cor.csv', squeeze(pow_cor_r_ES(:,1,:)))
+% csvwrite('ES_alpha_p_cor.csv', squeeze(pow_cor_r_ES(:,2,:)))
+% csvwrite('ES_beta__p_cor.csv', squeeze(pow_cor_r_ES(:,3,:)))
+% 
+% csvwrite('NS_theta_p_cor.csv', squeeze(pow_cor_r_NS(:,1,:)))
+% csvwrite('NS_alpha_p_cor.csv', squeeze(pow_cor_r_NS(:,2,:)))
+% csvwrite('NS_beta__p_cor.csv', squeeze(pow_cor_r_NS(:,3,:)))
+% 
+% % ISPC/PLV
+% csvwrite('R3_theta_ispc.csv', squeeze(ISPC_RS3(:,1,:)))
+% csvwrite('R3_alpha_ispc.csv', squeeze(ISPC_RS3(:,2,:)))
+% csvwrite('R3_beta__ispc.csv', squeeze(ISPC_RS3(:,3,:)))
+% 
+% csvwrite('R2_theta_ispc.csv', squeeze(ISPC_RS2(:,1,:)))
+% csvwrite('R2_alpha_ispc.csv', squeeze(ISPC_RS2(:,2,:)))
+% csvwrite('R2_beta__ispc.csv', squeeze(ISPC_RS2(:,3,:)))
+% 
+% csvwrite('R1_theta_ispc.csv', squeeze(ISPC_RS1(:,1,:)))
+% csvwrite('R1_alpha_ispc.csv', squeeze(ISPC_RS1(:,2,:)))
+% csvwrite('R1_beta__ispc.csv', squeeze(ISPC_RS1(:,3,:)))
+% 
+% csvwrite('ES_theta_ispc.csv', squeeze(ISPC_ES(:,1,:)))
+% csvwrite('ES_alpha_ispc.csv', squeeze(ISPC_ES(:,2,:)))
+% csvwrite('ES_beta__ispc.csv', squeeze(ISPC_ES(:,3,:)))
+% 
+% csvwrite('NS_theta_ispc.csv', squeeze(ISPC_NS(:,1,:)))
+% csvwrite('NS_alpha_ispc.csv', squeeze(ISPC_NS(:,2,:)))
+% csvwrite('NS_beta__ispc.csv', squeeze(ISPC_NS(:,3,:)))
+% 
+% %   amplitude envelope cor
+% csvwrite('R3_theta_amenv.csv', squeeze(am_env_RS3(:,1,:)))
+% csvwrite('R3_alpha_amenv.csv', squeeze(am_env_RS3(:,2,:)))
+% csvwrite('R3_beta__amenv.csv', squeeze(am_env_RS3(:,3,:)))
+% 
+% csvwrite('R2_theta_amenv.csv', squeeze(am_env_RS2(:,1,:)))
+% csvwrite('R2_alpha_amenv.csv', squeeze(am_env_RS2(:,2,:)))
+% csvwrite('R2_beta__amenv.csv', squeeze(am_env_RS2(:,3,:)))
+% 
+% csvwrite('R1_theta_amenv.csv', squeeze(am_env_RS1(:,1,:)))
+% csvwrite('R1_alpha_amenv.csv', squeeze(am_env_RS1(:,2,:)))
+% csvwrite('R1_beta__amenv.csv', squeeze(am_env_RS1(:,3,:)))
+% 
+% csvwrite('ES_theta_amenv.csv', squeeze(am_env_ES(:,1,:)))
+% csvwrite('ES_alpha_amenv.csv', squeeze(am_env_ES(:,2,:)))
+% csvwrite('ES_beta__amenv.csv', squeeze(am_env_ES(:,3,:)))
+% 
+% csvwrite('NS_theta_amenv.csv', squeeze(am_env_NS(:,1,:)))
+% csvwrite('NS_alpha_amenv.csv', squeeze(am_env_NS(:,2,:)))
+% csvwrite('NS_beta__amenv.csv', squeeze(am_env_NS(:,3,:)))
+% 
 % 
 % 
